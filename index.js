41a5ff6c65fd143d380b32fbe6690d6df761c1e4
Julien Poissonnier <j.poissonnier@whatever-company.com>
2018-05-28 09:55:14 +0200

    feat: Use prettier

--- index.js
+++ index.js
@@ -1,32 +1,30 @@
 'use strict'
 
 module.exports = {
-	extends: ['standard', 'standard-react'],
-	plugins: ['react', 'promise'],
+	extends: [
+		'standard',
+		'standard-react',
+		'plugin:react/recommended',
+		'prettier',
+		'prettier/react',
+		'prettier/standard'
+	],
+	plugins: ['graphql', 'react', 'prettier', 'standard'],
+	parserOptions: {
+		sourceType: 'module',
+		ecmaFeatures: {
+			jsx: true
+		}
+	},
 	rules: {
-		indent: ['error', 'tab'],
-		'max-len': ['error', { code: 120, tabWidth: 2 }],
-		quotes: ['error', 'single', { allowTemplateLiterals: true, avoidEscape: true }],
-		semi: 'off',
-		'object-curly-spacing': ['error', 'always'],
-		'no-tabs': 'off',
-		'no-mixed-spaces-and-tabs': 'error',
-		'object-property-newline': 0,
-		'babel/semi': ['error', 'never'],
-		'no-extra-semi': 0,
-		'no-mixed-operators': 'off',
+		'prettier/prettier': 'error',
+		'react/prop-types': 'off',
+		'react/display-name': 'off',
+		'react/no-find-dom-node': 'off',
 		'no-useless-return': 'off',
-		'prefer-promise-reject-errors': 'warn',
-		'react/prop-types': 0,
-		'react/jsx-indent': [2, 'tab'],
-		'react/jsx-indent-props': [2, 'tab'],
-		'react/no-unused-prop-types': 1,
-		'standard/no-callback-literal': 'off',
-		'generator-star-spacing': 1,
-		'jsx-quotes': [2, 'prefer-single'],
 		'no-console': ['error', { allow: ['warn', 'error'] }],
 		'no-unused-vars': [
-			1,
+			'warn',
 			{
 				args: 'none',
 				vars: 'local',
