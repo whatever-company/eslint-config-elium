fef58224f133240125a137bc3403b1ee0f144a87
Brice Maron <b.maron@whatever-company.com>
2021-10-19 16:52:38 +0200

    chore: add rule for jsx bool

--- index.js
+++ index.js
@@ -13,6 +13,9 @@ module.exports = {
 			version: 'detect',
 		},
 	},
+	// "off" or 0 - turn the rule off
+    // "warn" or 1 - turn the rule on as a warning (doesn't affect exit code)
+    // "error" or 2 - turn the rule on as an error (exit code is 1 when triggered)
 	rules: {
 		// Don't use import/no-duplicates as it's quite slow and we don't use Flow
 		// https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/no-duplicates.md#when-not-to-use-it
@@ -23,6 +26,7 @@ module.exports = {
 		'react/prop-types': 0,
 		'react/display-name': 0,
 		'react/no-find-dom-node': 0,
+		'react/jsx-boolean-value': 2,
 		'no-useless-return': 0,
 		'no-console': [2, { allow: ['warn', 'error'] }],
 		'no-unused-vars': [
